93631609a0f643d6eca12d19d134a3fbbe77abc9
Abd-MOUSSAOUI <a.moussaoui05@gmail.com>
2019-03-06 14:48:45 +0100

    add basic views, waiting for design

---
+++ RiskUI/src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Login from '@/views/Login'
+import Register from '@/views/Register'
+import CreateGame from '@/views/CreateGame'
+import JoinGame from '@/views/JoinGame'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    { path: '/', name: 'Home', component: Home },
+    { path: '/Register', name: 'Register', component: Register },
+    { path: '/Login', name: 'Login', component: Login },
+    { path: '/CreateGame', name: 'CreateGame', component: CreateGame },
+    { path: '/JoinGame', name: 'JoinGame', component: JoinGame }
+
+  ]
+})
---
+++ RiskUI/src/views/Login.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="row my-5">
+    <div class="col-md-6 offset-md-3">
+      <h1> Welcome to Risk game </h1>
+      <div class="card">
+        <div class="card-body">
+          <div class="form-group text-center">
+            <router-link to="/CreateGame" tag="button" class="btn form-control btn-success">Create Game</router-link>
+          </div>
+          <div class="form-group text-center">
+            <router-link to="/JoinGame" tag="button" class="btn form-control btn-success">Join Game</router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+      pseudo: '',
+      login: '',
+      password: ''
+    }
+  },
+  methods: {
+    // registerUser() {
+    //   console.log(this.pseudo, this.login, this.password)
+    // }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
