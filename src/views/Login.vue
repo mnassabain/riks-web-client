f6613fd0ca424af862b9a2113b18095f3715c3a1
Abd-MOUSSAOUI <a.moussaoui05@gmail.com>
2019-03-06 13:24:31 +0100

    init

---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    <h2>New game</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+    }
+  }
+}
+</script>
---
+++ src/views/Register.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="row my-5">
+    <div class="col-md-6 offset-md-3">
+      <h1> Welcome to Risk game </h1>
+      <div class="card">
+        <div class="card-body">
+          <h3 class="text-center my-4">Register</h3>
+          <div class="form-group">
+           <input id="player-pseudo" type="text" placeholder="Pseudo" class="form-control">
+          </div>
+          <div class="form-group">
+           <input id="player-login" type="text" placeholder="Login" class="form-control">
+          </div>
+          <div class="form-group">
+           <input id="player-login-conf" type="text" placeholder="Confirm Login" class="form-control">
+          </div>
+          <div class="form-group">
+           <input id="psswd" type="password" placeholder="Password" class="form-control">
+          </div>
+          <div class="form-group">
+           <input id="psswd-conf" type="password" placeholder="Confirm Password" class="form-control">
+          </div>
+          <div class="form-group text-center">
+            <button @click="registerUser()" class="btn form-control btn-success">Submit</button>
+            <router-link to="/" class="btn btn-link">Cancel</router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+        pseudo: '',
+        login: '',
+        password: ''
+        }
+  },
+  methods: {
+    registerUser() {
+      console.log(this.pseudo, this.login, this.password)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
