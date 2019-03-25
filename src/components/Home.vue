1e2f2c4ba52f1764c6fb42f9ffc324cb9a87e965
Thomas Millot <thomas.millot@etu.unistra.fr>
2019-03-25 14:30:05 +0100

    Register page redesign

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,8 +6,8 @@
       <h2>The game of global domination</h2>
     </div>
     <div class="input-block">
-      <input v-model="login" type="text" placeholder="LogIn">
-      <input v-model="password" type="password" placeholder="Password">
+      <input id="user" type="text" placeholder="User Name">
+      <input id="password" type="password" placeholder="Password">
     </div>
     <div class="login-bu">
       <router-link
@@ -19,7 +19,7 @@
     </div>
     <div class="register-block">
       <h3>Not registered yet?</h3>
-      <router-link :to="{ path: '/Register' }" class="button register-button my-1">Register</router-link>
+      <router-link :to="{ path: '/Register' }" tag="button" class="button register-button my-1">Register</router-link>
     </div>
   </div>
 </body>
