95bd888fe26d5380707be1d0d414665e01e3bb9f
Freddy <nawfal.freddy@gmail.com>
2019-03-30 18:52:11 +0100

    added Map structure

---
+++ src/js/Map.js
@@ -0,0 +1,180 @@
+
+
+export var map = {
+    africa: {
+        east_africa: {
+            player: false, // Put the id of the player owning this territory
+            soldiers: 0
+        },
+        egypt: {
+            player: false, 
+            soldiers: 0
+        },
+        congo: {
+            player: false, 
+            soldiers: 0
+        },
+        madagascar: {
+            player: false, 
+            soldiers: 0
+        },
+        south_africa: {
+            player: false, 
+            soldiers: 0
+        },
+        north_africa: {
+            player: false, 
+            soldiers: 0
+        }
+    },
+    asia: {
+        afghanistan: {
+            player: false, 
+            soldiers: 0
+        },
+        india: {
+            player: false, 
+            soldiers: 0
+        },
+        irkutsk: {
+            player: false, 
+            soldiers: 0
+        },
+        kamchatka: {
+            player: false, 
+            soldiers: 0
+        },
+        middle_east: {
+            player: false, 
+            soldiers: 0 
+        },
+        mongolia: {
+            player: false, 
+            soldiers: 0
+        },
+        siam: {
+            player: false, 
+            soldiers: 0
+        },
+        china: {
+            player: false, 
+            soldiers: 0
+        },
+        japan: {
+            player: false, 
+            soldiers: 0
+        },
+        siberia: {
+            player: false, 
+            soldiers: 0
+        },
+        ural: {
+            player: false, 
+            soldiers: 0
+        },
+        yakutsk: {
+            player: false, 
+            soldiers: 0
+        }
+    },
+    australia: {
+        eastern_australia: {
+            player: false, 
+            soldiers: 0
+        },
+        new_guinea: {
+            player: false, 
+            soldiers: 0
+        },
+        western_australia: {
+            player: false, 
+            soldiers: 0
+        },
+        indonesia: {
+            player: false, 
+            soldiers: 0
+        }
+    },
+    europe: {
+        great_britain: {
+            player: false, 
+            soldiers: 0
+        },
+        iceland: {
+            player: false, 
+            soldiers: 0
+        },
+        northern_europe: {
+            player: false, 
+            soldiers: 0
+        },
+        scandinavia: {
+            player: false, 
+            soldiers: 0
+        },
+        southern_europe: {
+            player: false, 
+            soldiers: 0
+        },
+        ukraine: {
+            player: false, 
+            soldiers: 0
+        },
+        western_europe: {
+            player: false, 
+            soldiers: 0
+        }
+    },
+    north_america: {
+        alaska: {
+            player: false, 
+            soldiers: 0
+        },
+        alberta: {
+            player: false, 
+            soldiers: 0
+        },
+        central_america: {
+            player: false, 
+            soldiers: 0
+        },
+        eastern_united_states: {
+            player: false, 
+            soldiers: 0
+        },
+        greenland: {
+            player: false, 
+            soldiers: 0
+        }, 
+        northwest_territory: {
+            player: false, 
+            soldiers: 0
+        },  
+        ontario: {
+            player: false, 
+            soldiers: 0
+        },
+        Western_united_states: {
+            player: false, 
+            soldiers: 0
+        },
+        quebec: {
+            player: false, 
+            soldiers: 0
+        }
+    },
+    south_america: {
+        argentina: {
+            player: false, 
+            soldiers: 0
+        },
+        peru: {
+            player: false, 
+            soldiers: 0
+        },
+        venezuela: {
+            player: false, 
+            soldiers: 0
+        }
+    }
+};
