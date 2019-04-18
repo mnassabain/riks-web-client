a277d9299457d6ac30b1ace5a820dcff45014f3e
MOUSAOUI MOHAMED <mohamed.mousaoui@etu.unistra.fr>
2019-04-18 20:01:21 +0200

    quick fix to test game flow (CAUTION must be improved)

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -5,12 +5,16 @@
       <h1>Room</h1>
     </div>
     <div class="list">
-      <ul v-for="item in items" :key="item.id">
+      <!--ul v-for="item in items" :key="item.id">
         <h3>{{ item.text }}</h3>
+      </ul-->
+      <ul v-for="(player) in players" :key="player.id">
+        <h3>{{ player }}</h3>
       </ul>
     </div>
     <div>
-      <button @click="startGame" tag="button" class="button validate-button my-1">Start the Game</button>
+       <button @click="leaveGame" tag="button" class="button second-button my-1">Cancel</button>
+      <!-- <router-link to="/MainMenu" tag="button" class="button second-button my-1">Cancel</router-link> -->
     </div>
     <div class="additional-button-block">
       <router-link to="/MainMenu" tag="button" class="button second-button my-1">Cancel</router-link>
@@ -20,22 +24,71 @@
 </template>
 
 <script>
+import {Packet} from "../Packet.js";
 
 export default {
   name: 'Start',
   data () {
     return {
-      items: [
-        { id: 0, text: 'Player 1' },
-        { id: 1, text: 'Player 2' },
-        { id: 2, text: 'Player 3' }
-      ]
+      players: [],
+      // items: [
+      //   { id: 0, text: 'Player 1' },
+      //   { id: 1, text: 'Player 2' },
+      //   { id: 2, text: 'Player 3' }
+      // ]
     }
   },
+  // CAUTION the following must to be improved
+  // TODO change button clics and messages handling behaviour
   methods: {
     startGame () {
-    }
-  }
+      this.$socket.send(new Packet("START_GAME").getJson());
+      console.log('send START_GAME')      
+    },
+
+   leaveGame(){
+       this.$socket.send(new Packet("LEAVE_GAME").getJson());
+       console.log('send LEAVE_GAME')
+       this.$router.push({path: "\MainMenu"})
+       delete vm.$socket.onmessage;
+   }
+  },
+  created(){
+     var vm = this;
+     this.$socket.send(new Packet("LOBBY_STATE").getJson());
+     console.log("send LOBBY_STATE");
+        
+     this.$socket.onmessage = function(d){
+        console.log(d);
+        var msg = JSON.parse(d.data);
+        if(msg.data.error){
+            // TODO: handle error
+            console.log("ERROR");
+            console.log(msg)
+            if(msg.type == 14)
+              vm.$router.push({path: "\DynamicGameWindow"})
+
+            delete vm.$socket.onmessage;
+        }
+        else{
+            if(msg.type == new Packet("LOBBY_STATE").type){
+                console.log("recv LOBBY_STATE");
+                vm.players = msg.data.gameData.playerNames
+                console.log(msg.data.gameData)
+            }else if(msg.type == new Packet("START_GAME").type){
+                console.log("recv START_GAME");
+                //vm.players = msg.data.gameData.playerNames
+                console.log(msg.data.gameData)
+                this.$router.push({path: "\DynamicGameWindow"})
+            }else if(msg.type == new Packet("GAME_STATUS").type){
+                console.log("recv GAME_STATUS");
+                //vm.players = msg.data.gameData.playerNames
+                console.log(msg.data.gameData)
+                this.$router.push({path: "\DynamicGameWindow"})
+            }
+        }
+     }
+   }
 }
 </script>
 
