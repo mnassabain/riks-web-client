a7ebba48129df2c1351554e793139486697eddcc
MOUSAOUI MOHAMED <mohamed.mousaoui@etu.unistra.fr>
2019-04-20 18:09:24 +0200

    Changed order of territories to match server's terId order

--- src/js/Map.js
+++ src/js/Map.js
@@ -1,7 +1,93 @@
 export var map = {
+    'North America': {
+        'Alaska': {
+            player: false, 
+            soldiers: 0
+        }, 
+        'Northwest Territory': {
+            player: false, 
+            soldiers: 0
+        },
+        'Greenland': {
+            player: false, 
+            soldiers: 0
+        },
+        'Alberta': {
+            player: false, 
+            soldiers: 0
+        },
+        'Ontario': {
+            player: false, 
+            soldiers: 0
+        },
+        'Quebec': {
+            player: false, 
+            soldiers: 0
+        },
+        'Eastern United States': {
+            player: false, 
+            soldiers: 0
+        },
+        'Western United States': {
+            player: false, 
+            soldiers: 0
+        },
+        'Central America': {
+            player: false, 
+            soldiers: 0
+        },
+    },
+    'South America': {
+        'Venezuela': {
+            player: false, 
+            soldiers: 0
+        },
+        'Peru': {
+            player: false, 
+            soldiers: 0
+        },
+        'Brazil': {
+            player: false, 
+            soldiers: 0
+        },
+        'Argentina': {
+            player: false, 
+            soldiers: 0
+        },
+    },
+    'Europe': {
+        'Iceland': {
+            player: false, 
+            soldiers: 0
+        },
+        'Scandinavia': {
+            player: false, 
+            soldiers: 0
+        },
+        'Ukraine': {
+            player: false, 
+            soldiers: 0
+        },
+        'Great Britain': {
+            player: false, 
+            soldiers: 0
+        },
+        'Northern Europe': {
+            player: false, 
+            soldiers: 0
+        },
+        'Southern Europe': {
+            player: false, 
+            soldiers: 0
+        },
+        'Western Europe': {
+            player: false, 
+            soldiers: 0
+        }
+    },
     'Africa': {
-        'East Africa': {
-            player: false, // Put the id of the player owning this territory
+        'North Africa': {
+            player: false, 
             soldiers: 0
         },
         'Egypt': {
@@ -12,53 +98,21 @@ export var map = {
             player: false, 
             soldiers: 0
         },
-        'Madagascar': {
-            player: false, 
+        'East Africa': {
+            player: false, // Put the id of the player owning this territory
             soldiers: 0
         },
         'South Africa': {
             player: false, 
             soldiers: 0
         },
-        'North Africa': {
+        'Madagascar': {
             player: false, 
             soldiers: 0
-        }
+        },
     },
     'Asia': {
-        'Afghanistan': {
-            player: false, 
-            soldiers: 0
-        },
-        'India': {
-            player: false, 
-            soldiers: 0
-        },
-        'Irkutsk': {
-            player: false, 
-            soldiers: 0
-        },
-        'Kamchatka': {
-            player: false, 
-            soldiers: 0
-        },
-        'Middle East': {
-            player: false, 
-            soldiers: 0 
-        },
-        'Mongolia': {
-            player: false, 
-            soldiers: 0
-        },
-        'Siam': {
-            player: false, 
-            soldiers: 0
-        },
-        'China': {
-            player: false, 
-            soldiers: 0
-        },
-        'Japan': {
+        'Ural': {
             player: false, 
             soldiers: 0
         },
@@ -66,17 +120,49 @@ export var map = {
             player: false, 
             soldiers: 0
         },
-        'Ural': {
-            player: false, 
-            soldiers: 0
-        },
         'Yakutsk': {
             player: false, 
             soldiers: 0
-        }
+        },
+        'Kamchatka': {
+            player: false, 
+            soldiers: 0
+        },
+        'Afghanistan': {
+            player: false, 
+            soldiers: 0
+        },
+        'China': {
+            player: false, 
+            soldiers: 0
+        },
+        'Irkutsk': {
+            player: false, 
+            soldiers: 0
+        },
+        'Mongolia': {
+            player: false, 
+            soldiers: 0
+        },
+        'Japan': {
+            player: false, 
+            soldiers: 0
+        },
+        'Middle East': {
+            player: false, 
+            soldiers: 0 
+        },
+        'India': {
+            player: false, 
+            soldiers: 0
+        },
+        'Siam': {
+            player: false, 
+            soldiers: 0
+        },
     },
     'Australia': {
-        'Eastern Australia': {
+        'Indonesia': {
             player: false, 
             soldiers: 0
         },
@@ -84,101 +170,15 @@ export var map = {
             player: false, 
             soldiers: 0
         },
+        'Eastern Australia': {
+            player: false, 
+            soldiers: 0
+        },
         'Western Australia': {
             player: false, 
             soldiers: 0
         },
-        'Indonesia': {
-            player: false, 
-            soldiers: 0
-        }
-    },
-    'Europe': {
-        'Great Britain': {
-            player: false, 
-            soldiers: 0
-        },
-        'Iceland': {
-            player: false, 
-            soldiers: 0
-        },
-        'Northern Europe': {
-            player: false, 
-            soldiers: 0
-        },
-        'Scandinavia': {
-            player: false, 
-            soldiers: 0
-        },
-        'Southern Europe': {
-            player: false, 
-            soldiers: 0
-        },
-        'Ukraine': {
-            player: false, 
-            soldiers: 0
-        },
-        'Western Europe': {
-            player: false, 
-            soldiers: 0
-        }
-    },
-    'North America': {
-        'Alaska': {
-            player: false, 
-            soldiers: 0
-        },
-        'Alberta': {
-            player: false, 
-            soldiers: 0
-        },
-        'Central America': {
-            player: false, 
-            soldiers: 0
-        },
-        'Eastern United States': {
-            player: false, 
-            soldiers: 0
-        },
-        'Greenland': {
-            player: false, 
-            soldiers: 0
-        }, 
-        'Northwest Territory': {
-            player: false, 
-            soldiers: 0
-        },  
-        'Ontario': {
-            player: false, 
-            soldiers: 0
-        },
-        'Western United States': {
-            player: false, 
-            soldiers: 0
-        },
-        'Quebec': {
-            player: false, 
-            soldiers: 0
-        }
-    },
-    'South America': {
-        'Brazil': {
-            player: false, 
-            soldiers: 0
-        },
-        'Argentina': {
-            player: false, 
-            soldiers: 0
-        },
-        'Peru': {
-            player: false, 
-            soldiers: 0
-        },
-        'Venezuela': {
-            player: false, 
-            soldiers: 0
-        }
-    }
+    },    
 };
 
 /**
