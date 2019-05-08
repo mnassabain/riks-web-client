a57a87087ccd2456be9a9a10906e94bceffa6414
Thomas Millot <thomas.millot@etu.unistra.fr>
2019-05-08 17:44:50 +0200

    Game Over screen UI redesign

--- src/views/GameOver.vue
+++ src/views/GameOver.vue
@@ -1,64 +1,56 @@
 <template>
-    <div class="game-over-container">
-        <h1>{{ message }}</h1>
-        <h2>Winner: {{ winner }}</h2>
-        <button
-          @click="mainMenu()"
-          tag="button"
-          class="button validate-button my-1"
-        >Main Menu</button>
+<body class="center">
+  <div class="text-center col-lg-6">
+    <h1>{{ message }}</h1>
+    <img class="gameOverIllustration" :src="setSrcImg()">
+    <h2>Winner: {{ winner }}</h2>
+    <div class="my-5">
+      <button @click="mainMenu()" tag="button" class="button validate-button my-1">Main Menu</button>
     </div>
+  </div>
+</body>
 </template>
 
 <script>
 export default {
-    name: 'GameOver',
-    data() {
-        return {
-            winner: '',
-            message: '',
-        }
+  name: "GameOver",
+  data() {
+    return {
+      winner: "",
+      message: ""
+    };
+  },
+  methods: {
+    mainMenu: function() {
+      this.$router.push({ path: "/MainMenu" });
     },
-    methods: {
-        mainMenu: function(){
-            this.$router.push({ path: "/MainMenu" });
-        }
-    },
-    created() {
-        this.winner = localStorage.getItem('winner')
-        var myName = localStorage.getItem('login')
+    setSrcImg: function() {
+      this.winner = localStorage.getItem("winner");
+      var myName = localStorage.getItem("login");
 
-        if (myName === this.winner) {
-            this.message = 'Congratulations!'
-        }
-        else {
-            this.message = 'Game Over'
-        }
+      if (myName === this.winner) {
+        return require("@/assets/icons/medal.svg");
+      } else {
+        return require("@/assets/icons/grave.svg");
+      }
     }
-}
+  },
+  created() {
+    this.winner = localStorage.getItem("winner");
+    var myName = localStorage.getItem("login");
+
+    if (myName === this.winner) {
+      this.message = "Congratulations!";
+    } else {
+      this.message = "Game Over";
+    }
+  }
+};
 </script>
 
 <style src="../css/Menu.css" scoped></style>
 <style>
-
 body {
   background-color: #27282d;
-}
-
-.game-over-container 
-{
-    height: 100%;
-    width: 100%;
-    box-sizing: border-box;
-}
-
-.game-over-container h1
-{
-    color: white;
-}
-
-.game-over-container h2
-{
-    color: white;
 }
 </style>
