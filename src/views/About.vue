f1dcdf9813d88c5d26c62f40e468e34a7cd51049
Thomas Millot <thomas.millot@etu.unistra.fr>
2019-05-08 22:33:24 +0200

    Added about sceen content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,19 +1,87 @@
 <template>
 <body class="center">
-  <div class="text-center col-lg-6">
-    <div>
-      <h1>About</h1>
+  <div class="container">
+    <div class="row text-center">
+      <div class="col">
+        <div class="mb-3">
+          <h1>About</h1>
+        </div>
+      </div>
     </div>
-    <div></div>
-    <div class="additional-button-block">
-      <router-link to="/MainMenu" tag="button" class="button second-button my-1">Cancel</router-link>
+    <div class="row text-center">
+      <div class="col">
+        <h2>Client Development</h2>
+        <h5>Mohamed Mousaoui</h5>
+        <h5>Thomas Millot</h5>
+        <h5>Freddy Nawfal</h5>
+        <h5>Marco Nassabain</h5>
+        <h5>Adberahmane Moussaoui</h5>
+        <h5>Zhigang Peng</h5>
+        <p></p>
+        <h2>Server Development</h2>
+        <h5>Dimitri Mlotokowski</h5>
+        <h5>Yossef Rostaqi</h5>
+        <h5>Raphaël Schwehr</h5>
+        <h5>Marco Nassabain</h5>
+        <h5>Eddy Pambou Moubogha</h5>
+        <p></p>
+      </div>
+      <div class="col">
+        <h2>Client-Server Connexion</h2>
+        <h5>Marco Nassabain</h5>
+        <h5>Freddy Nawfal</h5>
+        <h5>Mohamed Mousaoui</h5>
+        <h5>Adberahmane Moussaoui</h5>
+        <h5>Raphaël Schwehr</h5>
+        <h5>Eddy Pambou Moubogha</h5>
+        <p></p>
+        <p></p>
+        <h2>Documentation</h2>
+        <h5>Raphaël Schwehr</h5>
+        <h5>Freddy Nawfal</h5>
+        <h5>Mohamed Mousaoui</h5>
+        <p></p>
+      </div>
+      <div class="col">
+        <h2>Database</h2>
+        <h5>Raphaël Schwehr</h5>
+        <h5>Zhigang Peng</h5>
+        <p></p>
+        <h2>Graphics, Visuals & Logo</h2>
+        <h4>Thomas Millot</h4>
+        <p></p>
+        <h2>Map by</h2>
+        <h5>
+          <a href="https://github.com/raddrick/risk-map-svg">Anders Riutta</a>
+        </h5>
+        <p></p>
+        <h2>Icons by</h2>
+        <h5>
+          <a href="https://www.freepik.com/">Freepik,</a>
+          <a href="https://www.flaticon.com/authors/prettycons">Prettycons,</a>
+        </h5>
+        <h5>
+          <a href="https://www.flaticon.com/authors/smashicons">Smashicons,</a>
+          <a href="https://www.flaticon.com/authors/becris">Becris,</a>
+        </h5>
+        <h5>
+          <a href="https://www.flaticon.com/authors/photo3idea-studio">Photo3idea-studio</a>
+        </h5>
+      </div>
+    </div>
+    <div class="row text-center">
+      <div class="col">
+        <div class="mt-3">
+          <router-link to="/MainMenu" tag="button" class="button second-button my-1">Main Menu</router-link>
+        </div>
+      </div>
     </div>
   </div>
 </body>
 </template>
 
 <script>
-import {Packet} from "../Packet.js";
+import { Packet } from "../Packet.js";
 
 export default {
   name: "About"
@@ -23,7 +91,18 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style src="../css/Menu.css" scoped></style>
 <style>
+html,
 body {
   background-color: #27282d;
+}
+
+a:link {
+  color: #fdf6eb;
+  background-color: transparent;
+}
+
+a:visited {
+  color: #fdf6eb;
+  background-color: transparent;
 }
 </style>
